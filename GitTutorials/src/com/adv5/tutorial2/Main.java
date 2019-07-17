4d5f779f785bfc901754b4cfa21504d51a736e44
Dang Manh Duc <ducdm2494@gmail.com>
2019-07-17 20:50:44 +0700

    Print something on main method

--- GitTutorials/src/com/adv5/tutorial2/Main.java
+++ GitTutorials/src/com/adv5/tutorial2/Main.java
@@ -3,6 +3,6 @@ package com.adv5.tutorial2;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+		System.out.println("I have made something here.");
     }
 }
